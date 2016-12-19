9cf2bd7ce2b67e28f8d10628568dca226aa64f43
ahoogerwerf <ahoogerwerf@cfpwood.com>
2016-12-19 11:33:01 -0400

    first

---
+++ jobData/routes/evupdate.js
@@ -0,0 +1,13 @@
+﻿var express = require('express');
+var router = express.Router();
+sse = require('express-server-sent-events');
+
+
+router.get('/', sse,  function (req, res) {
+    req.socket.setTimeout(100000);
+    // push the current session onto a array
+    rescollection.push(res);
+
+});
+
+module.exports = router;
---
+++ jobData/routes/tr/jobstain.js
@@ -0,0 +1,38 @@
+﻿var express = require('express');
+var router = express.Router();
+var sse = require('express-server-sent-events');
+var jobData = {};
+
+//
+
+// POst data
+
+router.post('/', sse, function (request, res) {
+
+	jobData.Mill = request.body.Mill;
+	jobData.Job = request.body.Job;
+    jobData.Stain = request.body.Stain;
+
+    
+        rescollection.forEach(function (item, index) {
+// for some reason, the push to the client only works from within a async call, and data: must be at the beginning                
+            setTimeout(function () {
+            item.sse('data: ' + JSON.stringify(jobData) + '\n\n');
+            }
+                , 0);
+
+        });
+   // }
+    
+
+});
+
+
+
+router.get('/', function (req, res) {
+    res.render('jobstain', { Mill: jobData.Mill, Job: jobData.Job, Stain: jobData.Stain });
+    res.end("yes");
+
+});
+
+module.exports = router;
